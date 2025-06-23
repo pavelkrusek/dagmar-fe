5f7a6d4d4fecd0ab78599a1d48e352638095b5e0
Pavel Krusek <pavel@krusek.dk>
2025-06-23 16:02:39 +0200

    chore: add tailwind, prettier, favicon.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
 
   modules: [
     '@nuxt/eslint',
@@ -10,6 +14,20 @@ export default defineNuxtConfig({
     '@nuxt/image',
     '@nuxt/scripts',
     '@nuxt/test-utils',
-    '@nuxt/ui'
-  ]
+    '@nuxt/ui',
+  ],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  app: {
+    head: {
+      title: "Hi, I'm Dagmar",
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      ],
+    },
+  },
 })
